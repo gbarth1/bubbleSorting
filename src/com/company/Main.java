fa4b6ce327955feeab6b0aaea90d56bd75365a6d
gbarth1 <grace.m.barth@gmail.com>
2017-02-08 14:30:28 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Tester {
+
+    public static void main(String args[]) {
+
+        int theArray[] = {4, 8, 15, 16, 23, 42};
+
+        sort(theArray);
+
+        for(int j = 0; j < theArray.length; j++) {
+
+            System.out.print(theArray[j] + " "); }
+
+        System.out.println(" "); }
+
+    public static void sort(int a[ ]) {
+
+//Bubble Sort
+
+        for(int i=0; i<6; i++){
+            
+        }
+
+//Selection Sort
+
+//Quick Sort
+
+//Merge Sort
+
+    }
+
+}
